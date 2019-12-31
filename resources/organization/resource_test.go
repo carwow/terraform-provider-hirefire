ca60ca3818af3f5d3aa6ee3cc991828f35302619
Pedro Chambino <pchambino@gmail.com>
2019-12-31 13:15:54 +0000

    Resource testing helper and checkDestroy

--- resources/organization/resource_test.go
+++ resources/organization/resource_test.go
@@ -2,10 +2,9 @@ package organization_test
 
 import (
 	"fmt"
-	"os"
 	"testing"
 
-	"github.com/carwow/terraform-provider-hirefire/provider"
+	"github.com/carwow/terraform-provider-hirefire/testing/helper"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
@@ -15,17 +14,13 @@ func TestAccImportBasic(t *testing.T) {
 	name := fmt.Sprintf("test-%s", acctest.RandString(10))
 
 	resource.Test(t, resource.TestCase{
-		PreCheck: func() {
-			if os.Getenv("HIREFIRE_API_KEY") == "" {
-				t.Fatal("HIREFIRE_API_KEY must be set")
-			}
-		},
-		Providers: map[string]terraform.ResourceProvider{
-			"hirefire": provider.Provider(),
-		},
+		PreCheck:     helper.PreCheck(t),
+		Providers:    helper.Providers(),
+		CheckDestroy: checkDestroy,
 		Steps: []resource.TestStep{
 			{
 				Config: config(name, "UTC"),
+				Check:  resource.ComposeTestCheckFunc(),
 			},
 			{
 				ResourceName:      "hirefire_organization.foobar",
@@ -42,4 +37,22 @@ resource "hirefire_organization" "foobar" {
     name = "%s"
     time_zone = "%s"
 }`, name, timeZone)
+}
+
+func checkDestroy(s *terraform.State) error {
+	client := helper.Client()
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "hirefire_organization" {
+			continue
+		}
+
+		_, err := client.Organization.Get(rs.Primary.ID)
+
+		if err == nil {
+			return fmt.Errorf("Still exists")
+		}
+	}
+
+	return nil
 }
