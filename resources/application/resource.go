5ac7d283aa76d8592ca6e41aae4cd857d570a001
Lee Trout <ltrout@crunchtime.com>
2023-10-19 12:20:33 -0400

    Stub in token support

--- resources/application/resource.go
+++ resources/application/resource.go
@@ -64,6 +64,7 @@ func setAttributes(d *schema.ResourceData, app *client.Application) {
 	d.Set("restart_crashed_dynos", app.RestartCrashedDynos)
 	d.Set("new_issue_notifications", app.NewIssueNotifications)
 	d.Set("resolved_issue_notifications", app.ResolvedIssueNotifications)
+	d.Set("token", app.Token)
 }
 
 func getAttributes(d *schema.ResourceData) client.Application {
@@ -101,6 +102,11 @@ func getAttributes(d *schema.ResourceData) client.Application {
 	if v, ok := d.GetOk("resolved_issue_notifications"); ok {
 		value := v.(bool)
 		app.ResolvedIssueNotifications = value
+	}
+
+	if v, ok := d.GetOk("token"); ok {
+		value := v.(string)
+		app.Token = value
 	}
 
 	return app
